f6ebd1d985618f5c2abbfb171237833807099841
Jeroen Dijkstra <jeroen1066@gmail.com>
2024-05-14 22:18:52 +0200

    q2 now works as expected

--- ShapeOptimization/Question2analysis.py
+++ ShapeOptimization/Question2analysis.py
@@ -122,9 +122,9 @@ integrator_settings = integrator.runge_kutta_fixed_step_size(
 )
 
 propagator_settings.integrator_settings = integrator_settings
-propagator_settings.integrator_settings = propagation_setup.integrator.runge_kutta_fixed_step_size(
-    benchmark_step_size,
-    propagation_setup.integrator.CoefficientSets.rkf_56)
+#propagator_settings.integrator_settings = propagation_setup.integrator.runge_kutta_fixed_step_size(
+#    benchmark_step_size,
+#    propagation_setup.integrator.CoefficientSets.rkf_56)
 propagator_settings.print_settings.print_dependent_variable_indices = True
 
 benchmark_dynamics_simulator = numerical_simulation.create_dynamics_simulator(
@@ -158,8 +158,25 @@ available_propagators_names = ['cowell',
                                     'unified_state_model_modified_rodrigues_parameters',
                                     'unified_state_model_exponential_map']
 
+cowell_variable_names = ['x', 'y', 'z', 'vx', 'vy', 'vz']
+encke_variable_names = ['a', 'e', 'i', 'RAAN', 'AOP', 'TA']
+gauss_keplerian_variable_names = ['a', 'e', 'i', 'RAAN', 'AOP', 'TA']
+gauss_modified_equinoctial_variable_names = ['semiParameter', 'h1', 'h2', 'k1', 'k2', 'trueLongitude']
+unified_state_model_quaternions_variable_names = ['x', 'y', 'z', 'q1', 'q2', 'q3', 'q4']
+unified_state_model_modified_rodrigues_parameters_variable_names = ['x', 'y', 'z', 'q1', 'q2', 'q3', 'q4']
+unified_state_model_exponential_map_variable_names = ['x', 'y', 'z', 'q1', 'q2', 'q3', 'q4']
+
+variable_names = [cowell_variable_names,
+                     encke_variable_names,
+                     gauss_keplerian_variable_names,
+                     gauss_modified_equinoctial_variable_names,
+                     unified_state_model_quaternions_variable_names,
+                     unified_state_model_modified_rodrigues_parameters_variable_names,
+                     unified_state_model_exponential_map_variable_names]
+
 
 errors_for_propagators = []
+propagated_states = []
 
 for propagator in available_propagators:
     propagator_settings = Util.get_propagator_settings(shape_parameters,
@@ -178,23 +195,29 @@ for propagator in available_propagators:
     )
 
     propagator_settings.integrator_settings = integrator_settings
-    propagator_settings.integrator_settings = propagation_setup.integrator.runge_kutta_fixed_step_size(
-        benchmark_step_size,
-        propagation_setup.integrator.CoefficientSets.rkf_56)
+    #propagator_settings.integrator_settings = propagation_setup.integrator.runge_kutta_fixed_step_size(
+    #    benchmark_step_size,
+    #    propagation_setup.integrator.CoefficientSets.rkf_56)
     
     dynamics_simulator = numerical_simulation.create_dynamics_simulator(
         bodies,
         propagator_settings )
     
     state_history = dynamics_simulator.state_history
+    states_as_propagated = dynamics_simulator.unprocessed_state_history
+    propagated_states.append(states_as_propagated)
     errors = {}
+    end = list(state_history.keys())[-1]
     for epoch in state_history.keys():
+        if epoch > end - 10 * benchmark_step_size:
+            continue
         benchmark_state = benchmark_interpolator.interpolate(epoch)
         state = state_history[epoch]
         difference = np.linalg.norm(benchmark_state - state)
         errors[epoch] = difference
 
     errors_for_propagators.append(errors)
+
 
 ###########################################################################
 # PLOT RESULTS ############################################################
@@ -208,4 +231,22 @@ plt.xlabel('Time [s]')
 plt.ylabel('Difference')
 plt.title('Difference between benchmark and regular simulation')
 plt.legend()
+plt.grid()
 plt.show()
+
+
+
+for i in range(len(available_propagators)):
+    unprocessed_state = propagated_states[i]
+    times = list(unprocessed_state.keys())
+    states = np.array(list(unprocessed_state.values()))
+    state_names = variable_names[i]
+    fig,axs = plt.subplots(2,3)
+    for j in range(6):
+        state = states[:,j]
+        axs[j//3,j%3].plot(times,state)
+        axs[j//3,j%3].set_title('State ' +state_names[j])
+        axs[j//3,j%3].grid()
+        fig.suptitle(available_propagators_names[i])
+        #axs[j//2,j%2].title = available_propagators_names[i]
+    plt.show()
