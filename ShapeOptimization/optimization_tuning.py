7ba60fc96f5a9a9e691ee5fc151cbbafdb21214b
jpedra <jpedra@student.tudelft.nl>
2024-06-21 19:37:25 +0200

    tuning implementation

--- ShapeOptimization/optimization_tuning.py
+++ ShapeOptimization/optimization_tuning.py
@@ -13,7 +13,7 @@ from tudatpy.kernel.math import interpolators
 
 # Problem-specific imports
 import CapsuleEntryUtilities as Util
-import Optimizationutilities as OptUtil
+import Optimizationutilities_tuning as OptUtil
 
 # General python imports
 import numpy as np
@@ -31,10 +31,18 @@ spice_interface.load_standard_kernels()
 #seeds needs to be equal size to num_repeats
 
 optimizer_name = 'moead_gen'
-num_repeats = 4
-num_generations = 25
-num_pops = 45
-seeds = [42, 84, 144, 169]
+nominal_generations = 25
+nominal_populations = 45
+nominal_neighbours = 20
+nominal_CR = 1
+nominal_F = 0.5
+nominal_eta_m = 20
+nominal_realb = 0.9
+
+seeds_to_test = [42, 84, 144, 169]
+generations_to_test = [25, 50, 75, 100, 150]
+pops_to_test = [30, 45, 60, 75, 100, 150]
+num_repeats = len(seeds_to_test)
 
 range_per_parameter = [[0,5],
                        [2,5],
@@ -47,21 +55,26 @@ range_per_parameter = [[0,5],
 
 bounds = [[0,2,0,-np.pi/2,2.5,0.0,50],[5,5,3,0,5.5,0.5,400]]
 
-opt = OptUtil.optimization(bounds, optimizer_name)
-opt.optimize(num_pops,num_generations,num_repeats,seeds)
-results = opt.results
-results_per_generation = opt.results_per_generation
 
-results_to_store = []
+for test_seed in seeds_to_test:
+    print('Testing seed: ', test_seed)
+    opt = OptUtil.optimization(bounds, optimizer_name)
+    opt.optimize(numpops = nominal_populations,
+                 numgens = nominal_generations,
+                 seed = test_seed, 
+                 numneighbours = nominal_neighbours, 
+                 test_CR = nominal_CR, 
+                 test_F = nominal_F, 
+                 test_eta_m = nominal_eta_m, 
+                 test_realb = nominal_realb)
 
-for i in range(num_repeats):
-    x = results[i].get_x()
-    y = results[i].get_f()
-    y_per_gen = results_per_generation[i]
+    results = opt.results
+    results_to_store = []
+    x = results.get_x()
+    y = results.get_f()
 
-    results_to_store.append([x,y,y_per_gen])
+    results_to_store.append([x,y])
 
-
-filename = 'results/' + optimizer_name + '.dat'
-file = open(filename,'wb')
-pickle.dump(results_to_store,file)
+    filename = 'results/tuning/' + optimizer_name + '_seed_' + str(test_seed) +'.dat' 
+    file = open(filename,'wb')
+    pickle.dump(results_to_store,file)
