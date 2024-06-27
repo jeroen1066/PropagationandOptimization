392f554053e815eb3cae1dbbab26a403afeb1af5
jpedra <jpedra@student.tudelft.nl>
2024-06-27 16:28:04 +0200

    small updates to 2b, still need to complete

--- ShapeOptimization/Plot_2b.py
+++ ShapeOptimization/Plot_2b.py
@@ -5,20 +5,20 @@ import math
 
 # from optimization_tuning_v1_maco import seeds_to_test,generations_to_test,pops_to_test,kernel_to_test,convergence_speed_to_test,threshold_to_test,std_convergence_speed_to_test,eval_stop_to_test,focus_to_test
 
-seeds_to_test = [42, 84, 144, 169, 74, 29, 60, 1745, 1480025]
-generations_to_test = [25, 50, 75, 100, 150]
-pops_to_test = [30, 50, 75, 100, 150]
+seeds_to_test = [42, 84, 144, 169, 74]
+generations_to_test = [10, 15, 25, 30, 40, 50]
+pops_to_test = [30, 50, 75, 100, 125]
 
-kernel_to_test = [10, 30, 50, 70, 90]
+kernel_to_test = [10, 15, 20, 25, 30]
 convergence_speed_to_test = [0.5, 1, 1.5, 2, 2.5]
 threshold_to_test = [1, 2,  5, 10, 20]
-std_convergence_speed_to_test = [3, 5, 7, 9, 11, 15, 21]
-eval_stop_to_test = [50000, 75000, 100000, 125000, 150000, 200000, 300000]
-focus_to_test = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.75, 1.0]
+std_convergence_speed_to_test = [3, 5, 7, 9, 11]
+eval_stop_to_test = [50, 75, 100, 125, 150]
+focus_to_test = [0.1,  0.3,  0.5, 0.7, 1.0]
 
 optimizer_name = ['maco']
 
-save_directory = 'ShapeOptimization/results/tuning_v1_maco/'
+save_directory = 'ShapeOptimization/results/tuning_v2_maco/'
 
 plot_seed = True
 plot_generations = True
@@ -33,6 +33,7 @@ plot_focus = True
 
 
 if plot_seed:
+    plt.figure()
     for i in range(len(seeds_to_test)):
         data_file = save_directory + 'maco_seed_' + str(seeds_to_test[i]) + '.dat'
         ancillary_file = save_directory + 'maco_seed_' + str(seeds_to_test[i]) + '_ancillary.txt'
@@ -41,12 +42,24 @@ if plot_seed:
         file.close()
         x = data[0]
         y = data[1]
-        time_sim = np.genfromtxt(ancillary_file)
-        plt.scatter(y[0],y[1],label='Seed '+str(seeds_to_test[i]))
+        y_per_gen = np.array(data[2])
+        avg_arr = np.zeros(y_per_gen.shape[0])
+        for k in range(y_per_gen.shape[0]):
+            gen_avg = 0
+            num_valid = 0
+            for l in range(y_per_gen.shape[1]):
+                avg_one_pop = np.sum(y_per_gen[k,l])/3
+                if avg_one_pop < 1000:
+                    gen_avg += avg_one_pop
+                    num_valid += 1
 
-    plt.xlabel('Objective 1')
-    plt.ylabel('Objective 2')
-    plt.title('Seed')
+
+        time_sim = np.genfromtxt(ancillary_file)
+        # plt.scatter(y[0],y[1],label='Seed '+str(seeds_to_test[i]))
+        #average_fitness = np.sum(y_arr,axis=1)/3
+
+        plt.plot(avg_arr, label='Seed: '+str(seeds_to_test[i]))
+
+    plt.grid()
     plt.legend()
     plt.show()
-
