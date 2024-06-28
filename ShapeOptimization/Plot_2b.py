ba71db7e67a460516f124ed67dac47e90f289e0d
jpedra <jpedra@student.tudelft.nl>
2024-06-28 17:06:31 +0200

    fixes

--- ShapeOptimization/Plot_2b.py
+++ ShapeOptimization/Plot_2b.py
@@ -20,16 +20,16 @@ optimizer_name = ['maco']
 
 save_directory = 'ShapeOptimization/results/tuning_v2_maco/'
 
-plot_seed = False
-plot_generations = False
-plot_pops = False
+plot_seed = True
+plot_generations = True
+plot_pops = True
 
-plot_kernel = False
-plot_convergence_speed = False
+plot_kernel = True
+plot_convergence_speed = True
 plot_threshold = False
-plot_std_convergence_speed = False
-plot_eval_stop = False
-plot_focus = False
+plot_std_convergence_speed = True
+plot_eval_stop = True
+plot_focus = True
 
 if plot_seed:
     fig, axs = plt.subplots(2,2, figsize=(8,6))
@@ -670,7 +670,68 @@ if plot_simulation_speed:
     plt.tight_layout()
     plt.yscale('log')
     plt.xticks(np.arange(len(x_values)), x_values.astype(int))
-    plt.show()
-    # plt.savefig('simulation_speed_tuning.png')
+    # plt.show()
+    plt.savefig('simulation_speed_tuning.png')
 
-        
+plot_tuned_results = True
+
+if plot_tuned_results:
+    fig, axs = plt.subplots(2,2, figsize=(8,6))
+    for i in range(len(seeds_to_test)):
+        data_file = save_directory + 'maco_tuned_seed_' + str(seeds_to_test[i]) + '.dat'
+        ancillary_file = save_directory + 'maco_tuned_seed_' + str(seeds_to_test[i]) + '_ancillary.txt'
+        file = open(data_file,'rb')
+        data = pickle.load(file)[0]
+        file.close()
+        x = data[0]
+        y = data[1]
+        y_per_gen = np.array(data[2])
+        # plot 1
+        axs[(0, 0)].scatter(np.asarray(y)[:,0], np.asarray(y)[:,1], label=str(seeds_to_test[i]) + ' tuned')
+        axs[(0, 0)].set_ylim([0, 8])
+        axs[(0, 0)].set_xlim([0, 8])
+        axs[(0, 0)].grid()
+        axs[(0, 0)].set_xlabel('mass fitness')
+        axs[(0, 0)].set_ylabel('l/d fitness')
+        axs[(0, 0)].legend()
+        # plot 2
+        axs[(0, 1)].scatter(np.asarray(y)[:, 0], np.asarray(y)[:, 2], label=str(seeds_to_test[i]) + ' tuned')
+        axs[(0, 1)].set_ylim([0, 8])
+        axs[(0, 1)].set_xlim([0, 8])
+        axs[(0, 1)].grid()
+        axs[(0, 1)].set_xlabel('mass fitness')
+        axs[(0, 1)].set_ylabel('g-load fitness')
+        axs[(0, 1)].legend()
+        # plot 3
+        axs[(1, 0)].scatter(np.asarray(y)[:, 1], np.asarray(y)[:, 2], label=str(seeds_to_test[i]) + ' tuned')
+        axs[(1, 0)].set_ylim([0, 8])
+        axs[(1, 0)].set_xlim([0, 8])
+        axs[(1, 0)].grid()
+        axs[(1, 0)].set_xlabel('l/d fitness')
+        axs[(1, 0)].set_ylabel('g-load fitness')
+        axs[(1, 0)].legend()
+        # plot 3
+        avg_arr = np.zeros(y_per_gen.shape[0])
+        for k in range(y_per_gen.shape[0]):
+            gen_avg = 0
+            num_valid = 0
+            for l in range(y_per_gen.shape[1]):
+                avg_one_pop = np.sum(y_per_gen[k,l])/3
+                if avg_one_pop < 1000:
+                    gen_avg += avg_one_pop
+                    num_valid += 1
+            avg_arr[k] = gen_avg / num_valid
+
+        time_sim = np.genfromtxt(ancillary_file)
+        # plt.scatter(y[0],y[1],label='Seed '+str(seeds_to_test[i]))
+        #average_fitness = np.sum(y_arr,axis=1)/3
+
+        axs[(1, 1)].plot(avg_arr, label='seed ' + str(seeds_to_test[i]))
+        axs[(1, 1)].grid()
+        axs[(1, 1)].set_xlabel('evolutions done')
+        axs[(1, 1)].set_ylabel('average fitness')
+        axs[(1, 1)].legend()
+    
+    # plt.show()
+    plt.tight_layout()
+    plt.savefig('tuned.png')     
